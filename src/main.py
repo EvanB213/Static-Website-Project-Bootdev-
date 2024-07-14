c8cc469609ed441082ebbf59450305f77c4bf172
EvanB213 <evanb2132@gmail.com>
2024-07-14 16:27:02 -0400

    Added copying static info to new directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from textnode import TextNode
+import os
+import shutil
+from copydir import copy_dir
+
+dir_public = "./public"
+dir_static = "./static"
 
 def main():
-    node = TextNode("dummy text", "bold")
-    node2 = TextNode("This is a text node", "italic","https://www.boot.dev")
-    print(node)
-    print(node2)
+    print("Deleting public directory")
+    if os.path.exists(dir_public):
+        shutil.rmtree(dir_public)
+    
+    print("Copying static files to public directory")
+    copy_dir(dir_static, dir_public)
 
 main()
