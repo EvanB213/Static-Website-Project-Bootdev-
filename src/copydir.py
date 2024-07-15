883611985faa56ddc77952fc86d25eba6fd35d3e
EvanB213 <evanb2132@gmail.com>
2024-07-14 20:21:09 -0400

    Added new content and index

--- src/copydir.py
+++ src/copydir.py
@@ -1,6 +1,8 @@
 import shutil
 import os
 
+from markdown_block import *
+
 def copy_dir(source_dir, dest_dir):
     if not os.path.exists(dest_dir):
         os.mkdir(dest_dir)
@@ -13,3 +15,29 @@ def copy_dir(source_dir, dest_dir):
             shutil.copy(from_path, dest_path)
         else:
             copy_dir(from_path, dest_path)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+    from_file = open(from_path, 'r')
+    from_markdown = from_file.read()
+    from_file.close()
+
+    temp_file = open(template_path, 'r')
+    template = temp_file.read()
+    temp_file.close()
+
+    node = markdown_to_html_node(from_markdown)
+    html = node.to_html()
+
+    title = extract_title(from_markdown)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    pass
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import os
 import shutil
-from copydir import copy_dir
+from copydir import copy_dir, generate_page
 
 dir_public = "./public"
 dir_static = "./static"
+dir_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory")
@@ -12,5 +14,8 @@ def main():
     
     print("Copying static files to public directory")
     copy_dir(dir_static, dir_public)
+
+    print("Generating page")
+    generate_page(os.path.join(dir_content, "index.md"), template_path, os.path.join(dir_public, "index.html"))  
 
 main()
